e7a4b4c033ce8896266721a185b42489a77af65e
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2017-10-09 14:26:00 +0700

    Connect Home component to Redux. Also fix tests to make the test pass.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,67 +1,78 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import GenerationFilter from './GenerationFilter';
 import PokemonCardList from './PokemonCardList';
+import * as pokemonActions from '../actions/pokemonActions';
 
 class Home extends Component {
-  
-    constructor(props) {
-      super(props);
-      
-      this.pokemonList = [
-        {
-          pokemonId: 1, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 1'
-        }, 
-        {
-          pokemonId: 2, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 2'
-        }, 
-        {
-          pokemonId: 3, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 3'
-        }, 
-        {
-          pokemonId: 4, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 4'
-        }, 
-        {
-          pokemonId: 5, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 5'
-        }, 
-        {
-          pokemonId: 6, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 6'
-        }, 
-        {
-          pokemonId: 7, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 7'
-        }, 
-        {
-          pokemonId: 8, 
-          pokemonImageUrl: 'http://lorempixel.com/200/200/', 
-          pokemonName: 'Pikachu 8'
-        }
-      ];
-    }
-  
-    render() {
-      return (
-        <div>
-          <Title />
-          <GenerationFilter />
-          <PokemonCardList pokemonList={this.pokemonList} />
-        </div>
-      );
-    }
+
+  constructor(props) {
+    super(props);
+
+    this.pokemonList = [
+      {
+        pokemonId: 1,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 1'
+      },
+      {
+        pokemonId: 2,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 2'
+      },
+      {
+        pokemonId: 3,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 3'
+      },
+      {
+        pokemonId: 4,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 4'
+      },
+      {
+        pokemonId: 5,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 5'
+      },
+      {
+        pokemonId: 6,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 6'
+      },
+      {
+        pokemonId: 7,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 7'
+      },
+      {
+        pokemonId: 8,
+        pokemonImageUrl: 'http://lorempixel.com/200/200/',
+        pokemonName: 'Pikachu 8'
+      }
+    ];
   }
-  
-  export default Home;
-  
+
+  componentDidMount() {
+    this.props.actions.fetchPokemon();
+  }
+
+  render() {
+    return (
+      <div>
+        <Title />
+        <GenerationFilter />
+        <PokemonCardList pokemonList={this.pokemonList} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = null;
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(pokemonActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Home from './Home';
 import { shallow } from 'enzyme';
+import buildStore from '../store';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
-  shallow(<Home />);
+  const store = buildStore();
+  const context = { store };
+
+  shallow(<Home />, { context });
 });
