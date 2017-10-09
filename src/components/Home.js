3aa74cd59cade247cf3847dad64d14e2edcdd1f7
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2017-10-09 14:34:10 +0700

    Removing dummy data, adding props from store state.

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,48 +11,6 @@ class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.pokemonList = [
-      {
-        pokemonId: 1,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 1'
-      },
-      {
-        pokemonId: 2,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 2'
-      },
-      {
-        pokemonId: 3,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 3'
-      },
-      {
-        pokemonId: 4,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 4'
-      },
-      {
-        pokemonId: 5,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 5'
-      },
-      {
-        pokemonId: 6,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 6'
-      },
-      {
-        pokemonId: 7,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 7'
-      },
-      {
-        pokemonId: 8,
-        pokemonImageUrl: 'http://lorempixel.com/200/200/',
-        pokemonName: 'Pikachu 8'
-      }
-    ];
   }
 
   componentDidMount() {
@@ -64,13 +22,15 @@ class Home extends Component {
       <div>
         <Title />
         <GenerationFilter />
-        <PokemonCardList pokemonList={this.pokemonList} />
+        <PokemonCardList pokemonList={this.props.pokemonList} />
       </div>
     );
   }
 }
 
-const mapStateToProps = null;
+const mapStateToProps = (state) => ({
+  pokemonList: state.pokemonList.pokemonList
+});
 const mapDispatchToProps = (dispatch) => ({
   actions: bindActionCreators(pokemonActions, dispatch)
 });
