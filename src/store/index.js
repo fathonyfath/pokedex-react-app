acaf93f9810041d75394c86ab5d29e0945fb70c6
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2017-10-09 23:32:42 +0700

    Removing logger, updating index, and updating README.

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,5 @@ import logger from 'redux-logger';
 import parentReducer from '../reducers'
 
 export default function buildStore(initialState) {
-  return createStore(parentReducer, initialState, applyMiddleware(thunk, logger));
+  return createStore(parentReducer, initialState, applyMiddleware(thunk));
 }
